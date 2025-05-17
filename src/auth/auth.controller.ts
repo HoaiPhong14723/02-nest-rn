ddaa262328d4b9aae49f172d48309d568653853b
NguyenHoaiPhong <nguyenhoaiphong14723@gmail.com>
2025-05-17 22:19:05 +0700

    [update] - Send email with Nestjs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './passport/local-auth.guard';
 import { Public } from '@/decorator/customize';
 import { register } from 'module';
 import { CreateAuthDto } from './dto/create-auth.dto';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly mailerService: MailerService,
+  ) {}
 
   // @Post("login")
   // create(@Body() createAuthDto: CreateAuthDto) {
@@ -20,7 +34,7 @@ export class AuthController {
   @Post('login')
   @Public()
   @UseGuards(LocalAuthGuard)
-  handleLogin(@Request() req){
+  handleLogin(@Request() req) {
     return this.authService.login(req.user);
   }
 
@@ -30,7 +44,16 @@ export class AuthController {
     return this.authService.handleRegister(registerDto);
   }
 
-
-
-
+  @Get('mail')
+  @Public()
+  testMail() {
+    this.mailerService.sendMail({
+      to: 'pn55399@gmail.com', // list of receivers
+      // from: 'noreply@nestjs.com', // sender address
+      subject: 'Testing Nest MailerModule ✔', // Subject line
+      text: 'welcome', // plaintext body
+      html: '<b>hello world</b>', // HTML body content
+    });
+    return 'test mail';
+  }
 }
